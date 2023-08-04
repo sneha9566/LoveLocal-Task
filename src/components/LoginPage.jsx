75a4c58eef46f30aa4e3d3d8bb975a515804c3d2
Sneha Rani <sneharani@Snehas-MacBook-Air.local>
2023-08-04 12:35:51 +0530

    login validation

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { setToken } from '../redux/authSlice';
 import { useHistory } from 'react-router-dom';
 import { setLoginStatus } from '../redux/authSlice';
+import { useToast } from '@chakra-ui/react';
 
 import { Box, Flex, Heading, Input, Button, FormControl, FormLabel, Card } from '@chakra-ui/react';
 
@@ -14,9 +15,20 @@ const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
-  
+    const toast = useToast();
 
     const handleLogin = async () => {
+      if (!username || !password) {
+        toast({
+          title: 'Error',
+          description: 'Please enter both username and password.',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+        return; 
+      }
+    
       try {
         const response = await fetch('https://fakestoreapi.com/auth/login', {
           method: 'POST',
@@ -24,23 +36,41 @@ const LoginPage = () => {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            username: 'mor_2314',
-            password: '83r5^_',
+            username: username,
+            password: password,
           }),
         });
-  
+    
         const data = await response.json();
-        console.log(data);
-        // dispatch(setToken(data.token));
-        dispatch(setLoginStatus(true));
-        // Save login status in localStorage
-        localStorage.setItem('isLoggedIn', 'true');
-        history.push('/category/All');
+    
+        if (response.ok && data.token) {
+          // dispatch(setToken(data.token));
+          dispatch(setLoginStatus(true));
+          // Save login status in localStorage
+          localStorage.setItem('isLoggedIn', 'true');
+          history.push('/category/All');
+        } else {
+          toast({
+            title: 'Error',
+            description: 'Wrong username or password. Please try again.',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          });
+        }
       } catch (error) {
         console.error('Error logging in:', error);
-        // Handle error here (e.g., show error message to user)
+        toast({
+          title: 'Error',
+          description: 'Wrong username or password. Please try again.',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
       }
     };
+    
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
